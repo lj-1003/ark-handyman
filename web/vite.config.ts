d76fab98b581b2e25af50812f1ebbaddb7f3f458
lj-1003 <levijustin24@gmail.com>
2026-08-25 14:05:24 -0500

    Build fixes.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/ark-handyman/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
