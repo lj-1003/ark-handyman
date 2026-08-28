4a8c54852c0ebf678068dff22ae55323f7cde359
lj-1003 <levijustin24@gmail.com>
2026-08-28 13:13:49 -0500

    Footer design changes.

--- web/app/components/Footer.tsx
+++ web/app/components/Footer.tsx
@@ -2,17 +2,18 @@ import headerLogo from '../assets/logo.png'
 function Footer() {
   
   return (
-    <div className="">
-      <div className="pl-20 pr-20 grid grid-cols-2 p-8 bg-blue-300">
-        <div className="flex h-32">
-          <img src={headerLogo} alt="logo"></img>
-        </div>
-        <div className="text-right">
+    <div className="p-8  bg-blue-300">
+      <div className="pl-7 pr-7 grid sm:grid-cols-2">
+        <div className="flex flex-col text-center sm:text-left justify-center">
           <h1 className="text-4xl">Contact</h1><br />
-          <p>316-660-4298</p>
-          <a href="mailto:hello@arkhandyman.net">hello@arkhandyman.net</a>
+          <p className='text-2xl'>316-660-4298</p>
+          <p><a className='text-2xl' href="mailto:hello@arkhandyman.net">hello@arkhandyman.net</a></p>
+        </div>
+        <div className="flex flex-row h-60 justify-center items-center sm:justify-end">
+          <img className='h-50 w-50' src={headerLogo} alt="logo"></img>
         </div>
       </div>
+      <h1 className='text-3xl text-center'>Ark Rennovation & Handyman LLC</h1>
     </div>
   );
 }
