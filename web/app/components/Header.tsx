b30dec4ee7bb259f8f386ef602ba7bc73371e919
lj-1003 <levijustin24@gmail.com>
2026-09-02 11:08:42 -0500

    Header update.

--- web/app/components/Header.tsx
+++ web/app/components/Header.tsx
@@ -3,6 +3,10 @@ function Header() {
   
   return (
     <div className='bg-blue-300 shadow-xl'>
+      <div className='p-2 flex flex-col items-center'>
+         <p className='text-2xl'>316-660-4298</p>
+         <p><a className='text-2xl' href="mailto:hello@arkhandyman.net">hello@arkhandyman.net</a></p>
+      </div>
       <div className="flex h-56 justify-center">
         <img src={headerLogo} alt="logo"></img>
       </div>
